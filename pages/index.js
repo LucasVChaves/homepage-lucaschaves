91f1ae180c0019fb0b67b35d6eafed9ad6a3ce6d
Lucas Chaves <lucasvchaves@gmail.com>
2023-07-01 11:39:48 -0300

    More info in the year 2023 at the bio

--- pages/index.js
+++ pages/index.js
@@ -61,7 +61,7 @@ const Page = () => {
                     </BioSection>
                     <BioSection>
                         <BioYear>2023</BioYear>
-                        Joined <a href="https://www.instagram.com/rinobotbrasil/" target="_blank">Rinobot Robotics Team</a> developing NAO humanoid robots. 🤖 
+                        Joined <a href="https://www.instagram.com/rinobotbrasil/" target="_blank">Rinobot Robotics Team</a> developing NAO humanoid robots. Had the honour to be a competitor at RoboCup 2023 in Bordeaux, France. 🤖 
                     </BioSection>
                 </Section>
 
